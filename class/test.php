24e1e9ed487adc35930b3abaf5525d5174429c8c
Denis Henriquez <denisadonis06@gmail.com>
2018-04-25 10:42:34 -0600

    pruebas con la base

--- class/test.php
+++ class/test.php
@@ -2,6 +2,8 @@
 <html>
 <head>
 	<title>test</title>
+	<meta charset="utf-8">
+	<link href="../css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 	<?php 
@@ -9,11 +11,35 @@
 
 		$conn = new Conexion();
 
-		if ($conn->getLink()) {
-			echo "conexion exitosa";
+		/*if ($conn->getLink()) {
+			echo "conexion exitosa<br>";
+			$cursor = $conn->ejecutarConsulta("SELECT * FROM Suscriptor");
+			while ($temp = $conn->obtenerFila($cursor)) {
+				echo utf8_encode($temp['Nombre']." ".$temp['Apellido']).'<br>';
+			}
 		} else {
 			echo "Fallo al intertar conectar";
-		}
+		}*/
 	?>
+	<table class="table table-hover">
+  		<thead>
+  			<tr>
+  				<th>Nombre</th>
+  				<th>Apellido</th>
+  			</tr>
+  		</thead>
+  		<tbody>
+   			<?php
+  				if ($conn->getLink()) {
+					echo "conexion exitosa<br>";
+					$cursor = $conn->ejecutarConsulta("SELECT * FROM Suscriptor");
+					while ($temp = $conn->obtenerFila($cursor)) {
+						echo '<tr><td>'.utf8_encode($temp['Nombre']).'</td>'.
+							'<td>'.utf8_encode($temp['Apellido']).'</td></tr>';
+					}
+				}
+  			?>
+  		</tbody>
+	</table>
 </body>
 </html>
